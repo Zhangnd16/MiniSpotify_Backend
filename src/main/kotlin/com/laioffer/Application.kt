31dbf0427717348311f940aa1811a78145b9c20d
Andy Zhang <zhangnd16@berkeley.edu>
2025-07-05 14:56:40 -0700

    feed, playlist retrieve

--- src/main/kotlin/com/laioffer/Application.kt
+++ src/main/kotlin/com/laioffer/Application.kt
@@ -4,10 +4,27 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.json.Json
+
+@Serializable
+data class Playlist (
+    val id: Long,
+    val songs: List<Song>
+)
+
+@Serializable
+data class Song (
+    val name: String,
+    val lyric: String,
+    val src: String,
+    val length: String
+)
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -20,17 +37,44 @@ fun Application.module() {
             prettyPrint = true
         })
     }
-    // TODO: adding the routing configuration here
+
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
 
-        get("/feed") {
-            call.respondText("feed")
+        get("feed") {
+            val jsonString: String? = this::class.java.classLoader.getResource("feed.json")?.readText()
+            call.respondText(jsonString ?: "null")
         }
-        get("/playlists") {
-            call.respondText("playlists")
+
+        get("playlists") {
+            val jsonString = this::class.java.classLoader.getResource("playlists.json")?.readText()
+            call.respondText(jsonString ?: "null")
+        }
+
+        get("playlist/{id}") {
+            this::class.java.classLoader.getResource("playlists.json")?.readText()?.let {
+                val playlists = Json.decodeFromString(ListSerializer(Playlist.serializer()), it)
+                val id = call.parameters["id"]
+                val playlist: Playlist? = playlists.firstOrNull { item: Playlist -> item.id.toString() == id }
+                call.respondNullable(playlist)
+            } ?: call.respondText("null")
+        }
+
+        static("/") {
+            staticBasePackage = "static"
+            static("songs") {
+                resources("songs")
+            }
         }
     }
+}
+
+fun myRouting(configuration: () -> Unit) {
+    configuration()
+}
+
+fun myGet(path: String, body: () -> Unit) {
+
 }
